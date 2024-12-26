58f50061116180c217d8408d194a0d299996f3a9
billybillydev <admin@billybilly.dev>
2024-12-26 21:13:24 +0100

    [feat]: implemented tag component

---
+++ apps/www/src/components/tag.component.jsx
@@ -0,0 +1,11 @@
+/**
+ * Tag component which is a wrapper for any html tag
+ * @param {import("$common/props").HTMLTagWithChildren<{ as: import("$common/types").HTMLTagNameType }>} props
+ */
+export function Tag({ as, children, ...restProps }) {
+	return (
+		<tag of={/** @type {string} */ (as)} {...restProps}>
+			{children}
+		</tag>
+	);
+}

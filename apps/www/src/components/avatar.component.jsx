31959a2d941f8eb374761f11b584c6e6297f52f5
billybillydev <admin@billybilly.dev>
2025-01-01 13:22:04 +0100

    [feat]: improved avatar component and remove tailwind config theme as dependency

--- apps/www/src/components/avatar.component.jsx
+++ apps/www/src/components/avatar.component.jsx
@@ -1,11 +1,13 @@
-import { tailwindThemeConfig } from "../common/tailwind-config";
-import { specificSizes } from "../common/types";
-import { Image } from "./image.component";
 import clsx from "clsx";
+import { Image } from "./image.component";
+
+/**
+ * @typedef {"6" | "7" | "8" | "9" | "10" | "11" | "12" | "14" | "16" | "20" | "24" | "28" | "32" | "36" | "40" | "44" | "48" | "52" | "56" | "60" | "64" | "72" | "80" | "96"} AvatarSize
+ */
 
 /**
  * @typedef AvatarProps
- * @type {{ image?: import("./image.component").ImageType, fallbackText?: string, color?: import("../common/types").ThemeColorType | { background: string, text: string }, size?: string} & import("../common/props").HTMLTag}
+ * @type {{ image?: import("./image.component").ImageType, fallbackText?: string, color?: import("../common/types").ThemeColorType | { background: string, text: string }, size?: AvatarSize} & import("../common/props").HTMLTag}
  */
 
 /**
@@ -13,11 +15,9 @@ import clsx from "clsx";
  * @type {import("../common/props").JSXComponent<AvatarProps>}
  */
 export function Avatar(props) {
-	const minSize = "8";
-	const { color, image, fallbackText, size = minSize } = props;
-	/**
-	 * @type {Map<import("../common/types").ThemeColorType, { background: string; text: string }>}
-	 */
+	const { color, image, fallbackText, size = "8" } = props;
+
+	/** @type {Map<import("../common/types").ThemeColorType, { background: string; text: string }>} */
 	const colorMap = new Map([
 		[
 			"primary",
@@ -59,33 +59,52 @@ export function Avatar(props) {
 			? colorMap.get(color)?.background
 			: color?.background ?? "bg-slate-200 border-slate-700";
 	const textColor =
-		typeof color === "string" ? colorMap.get(color)?.text : color?.text ?? "text-slate-700";
+		typeof color === "string"
+			? colorMap.get(color)?.text
+			: color?.text ?? "text-slate-700";
 
-	let width = tailwindThemeConfig.width[size];
-	let height = tailwindThemeConfig.height[size];
-
-	if (!specificSizes.includes(size)) {
-		console.warn(`size ${size} is not supported by tailwind`);
-		width = tailwindThemeConfig.width[minSize];
-		height = tailwindThemeConfig.height[minSize];
-	}
-	if (!Number(size) || Number(size) < Number(minSize)) {
-		width = tailwindThemeConfig.width[minSize];
-		height = tailwindThemeConfig.height[minSize];
-	}
+	/** @type {Map<AvatarSize, string>} */
+	const fontSizeMap = new Map([
+		["6", "text-xs"],
+		["7", "text-sm"],
+		["8", "text-sm"],
+		["9", "text-md"],
+		["10", "text-md"],
+		["11", "text-lg"],
+		["12", "text-lg"],
+		["14", "text-xl"],
+		["16", "text-xl"],
+		["20", "text-2xl"],
+		["24", "text-2xl"],
+		["28", "text-4xl"],
+		["32", "text-4xl"],
+		["36", "text-5xl"],
+		["40", "text-5xl"],
+		["44", "text-6xl"],
+		["48", "text-6xl"],
+		["52", "text-7xl"],
+		["56", "text-7xl"],
+		["60", "text-8xl"],
+		["64", "text-8xl"],
+		["72", "text-9xl"],
+		["80", "text-9xl"],
+		["96", "text-[12rem]"]
+	]);
 
 	return (
 		<div
 			class={clsx(
-				"flex items-center justify-center rounded-full overflow-hidden border max-w-xl",
+				"flex items-center justify-center rounded-full overflow-hidden border max-w-xl aspect-square",
+				`size-${size}`,
 				backgroundColor
 			)}
-			style={`width: ${width}; height: ${height};`}
 		>
 			{image ? (
-				<Image src={image.src} alt={image.alt} />
+				<Image src={image.src} alt={image.alt} class={"object-contain object-center w-full h-full"} />
 			) : (
-				<span class={textColor} safe>{fallbackText}</span>
+				<span class={clsx(textColor, fontSizeMap.get(size))} safe>
+					{fallbackText}
+				</span>
 			)}
 		</div>
 	);

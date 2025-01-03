974596d4a2cd3de7a28de3ac71baeaa5cbea251b
billybillydev <admin@billybilly.dev>
2025-01-03 10:17:15 +0100

    [feat]: implemented dynamic date year in footer

--- apps/www/src/views/footer.view.jsx
+++ apps/www/src/views/footer.view.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
     return (
         <footer class="p-2 border-t">
-            <p class="text-center">JSXPine 2024 © Copyright</p>
+            <p class="text-center">JSXPine <span x-text="new Date().getFullYear()" /> © Copyright</p>
         </footer>
     )
 }

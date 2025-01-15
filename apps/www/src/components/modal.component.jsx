61b708db4ee31da50490f9adc25a1ce1d24454d1
billybillydev <admin@billybilly.dev>
2025-01-15 08:31:51 +0100

    [fix]: remove modal x-trap focus attributes

--- apps/www/src/components/modal.component.jsx
+++ apps/www/src/components/modal.component.jsx
@@ -75,7 +75,6 @@ export function SimpleModalContent(props) {
 				/>
 				<div
 					x-bind="shower"
-					{...{ "x-trap.inert.noscroll": "show" }}
 					x-transition:enter="ease-out duration-300"
 					x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 					x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
